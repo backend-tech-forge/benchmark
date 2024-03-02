d3bf56016ae69feed266e967390d9109f963207e
Gyumin Hwangbo <ghkdqhrbals@gmail.com>
2024-03-02 19:33:47 +0900

    dev: change to eager fetch

--- bm-controller/src/main/java/org/benchmarker/user/model/User.java
+++ bm-controller/src/main/java/org/benchmarker/user/model/User.java
@@ -33,7 +33,7 @@ public class User extends BaseTime {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "group_id", referencedColumnName = "id", nullable = false)
     private UserGroup userGroup;
 
@@ -49,8 +49,16 @@ public class User extends BaseTime {
         this.role = role;
         this.userGroup = userGroup;
 
+        if (this.slackWebhookUrl == null) {
+            this.slackWebhookUrl = "";
+        }
+
+        if (this.email == null) {
+            this.email = "";
+        }
+
         if (this.role == null) {
-            this.role = Role.USER;
+            this.role = Role.ROLE_USER;
         }
         if (this.slackNotification == null) {
             this.slackNotification = false;
@@ -59,9 +67,20 @@ public class User extends BaseTime {
             this.emailNotification = false;
         }
     }
-
-
     public User() {
 
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", password='" + password + '\'' +
+                ", slackWebhookUrl='" + slackWebhookUrl + '\'' +
+                ", slackNotification=" + slackNotification +
+                ", email='" + email + '\'' +
+                ", emailNotification=" + emailNotification +
+                ", role=" + role +
+                '}';
     }
 }
