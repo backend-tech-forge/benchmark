0589a61a2a5609d03eb6c472c6f51863542f1c36
Gyumin Hwangbo <ghkdqhrbals@gmail.com>
2024-03-02 19:37:27 +0900

    dev: implement UserContext

---
+++ bm-controller/src/main/java/org/benchmarker/user/service/UserContext.java
@@ -0,0 +1,33 @@
+package org.benchmarker.user.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.benchmarker.security.BMUserDetails;
+import org.benchmarker.user.model.User;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Profile("production")
+@Component
+@Slf4j
+public class UserContext {
+    /**
+     * Get current user object from context.
+     *
+     * @return current user;
+     */
+    public User getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null) {
+            throw new AuthenticationCredentialsNotFoundException("No authentication");
+        }
+        Object obj = auth.getPrincipal();
+        if (!(obj instanceof BMUserDetails)) {
+            throw new AuthenticationCredentialsNotFoundException("Invalid authentication with " + obj);
+        }
+        BMUserDetails securedUser = (BMUserDetails) obj;
+        return securedUser.getUser();
+    }
+}
