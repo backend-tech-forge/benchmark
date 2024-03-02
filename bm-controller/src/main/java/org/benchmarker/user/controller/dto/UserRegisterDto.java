d704d5714e175048732a462fed5ef471d18d0fa9
Hwangbo <ghkdqhrbals@naver.com>
2024-03-02 19:51:59 +0900

    dev: implement UserRegisterDto

--- bm-controller/src/main/java/org/benchmarker/user/controller/dto/UserRegisterDto.java
+++ bm-controller/src/main/java/org/benchmarker/user/controller/dto/UserRegisterDto.java
@@ -1,16 +1,26 @@
-package org.benchmarker.user.model.dto;
+package org.benchmarker.user.controller.dto;
 
-import lombok.Builder;
-import lombok.Getter;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 import org.benchmarker.user.model.User;
 
 @Getter
-@Builder
-public class UserDto {
+@Setter
+@NoArgsConstructor
+public class UserRegisterDto {
+
+    @NotBlank
+    @Size(min = 4, max=20)
     private String id;
+    @NotBlank
+    @Size(min = 4, max=20)
     private String password;
+    @NotBlank
     private String slackWebhookUrl;
     private Boolean slackNotification;
+    @Email
     private String email;
     private Boolean emailNotification;
 
