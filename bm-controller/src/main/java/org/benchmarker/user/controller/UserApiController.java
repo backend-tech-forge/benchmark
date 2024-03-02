c10ea50043958dca980527c994b60de1ed652790
Hwangbo <ghkdqhrbals@naver.com>
2024-03-02 20:07:14 +0900

    dev: implement PreAuthorize and multiple restApi

--- bm-controller/src/main/java/org/benchmarker/user/controller/UserApiController.java
+++ bm-controller/src/main/java/org/benchmarker/user/controller/UserApiController.java
@@ -3,25 +3,51 @@ package org.benchmarker.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.benchmarker.user.controller.dto.UserInfo;
+import org.benchmarker.user.controller.dto.UserRegisterDto;
+import org.benchmarker.user.model.Role;
 import org.benchmarker.user.model.User;
-import org.benchmarker.user.model.dto.UserDto;
 import org.benchmarker.user.service.IUserService;
+import org.benchmarker.user.service.UserContext;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserApiController {
     private final IUserService userService;
+    private final UserContext userContext;
 
-    @PostMapping("/create")
-    public ResponseEntity createUser(@RequestBody UserDto userDto) {
-        User user = userDto.toEntity();
+    @PostMapping("/user")
+    public ResponseEntity createUser(@RequestBody UserRegisterDto userRegisterDto) {
+        User user = userRegisterDto.toEntity();
         return ResponseEntity.ok(userService.createUser(user));
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<UserInfo> getUser(@RequestParam String id) {
+        User user = userService.getUser(id);
+        return ResponseEntity.ok(UserInfo.from(user));
+    }
+
+    @GetMapping("/user/me")
+    public ResponseEntity<UserInfo> getUser() {
+        User currentUser = userContext.getCurrentUser();
+        return ResponseEntity.ok(UserInfo.from(currentUser));
+    }
+
+    @GetMapping("/users")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<List<UserInfo>> getUsers() {
+        List<User> users = userService.getUsers();
+        List<UserInfo> userInfo = users.stream().map(UserInfo::from).collect(Collectors.toList());
+        return ResponseEntity.ok(userInfo);
     }
 }
