2b503884f94a9ab14e57acb63a636c48d95cf03e
Hwangbo <ghkdqhrbals@naver.com>
2024-03-02 20:02:55 +0900

    dev: add List user return method

--- bm-controller/src/main/java/org/benchmarker/user/service/IUserService.java
+++ bm-controller/src/main/java/org/benchmarker/user/service/IUserService.java
@@ -2,11 +2,13 @@ package org.benchmarker.user.service;
 
 import org.benchmarker.user.model.User;
 
-import java.util.Optional;
+import java.util.*;
+
 
 public interface IUserService {
     Optional<User> createUser(User user);
-    Optional<User> getUser(String id);
+    User getUser(String id);
+    List<User> getUsers();
     Optional<User> updateUser(User user) throws Exception;
     void deleteUser(String id);
 }
