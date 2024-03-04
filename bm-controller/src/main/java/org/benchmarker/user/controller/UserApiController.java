38101d193fdb8ee50e6251f4c070d0efe9dcaa01
Hwangbo <ghkdqhrbals@naver.com>
2024-03-05 05:13:16 +0900

    dev: change userInfo get method with role based branch state

--- bm-controller/src/main/java/org/benchmarker/user/controller/UserApiController.java
+++ bm-controller/src/main/java/org/benchmarker/user/controller/UserApiController.java
@@ -1,11 +1,11 @@
 package org.benchmarker.user.controller;
 
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.benchmarker.user.controller.dto.UserInfo;
 import org.benchmarker.user.controller.dto.UserRegisterDto;
-import org.benchmarker.user.model.Role;
 import org.benchmarker.user.model.User;
 import org.benchmarker.user.service.IUserService;
 import org.benchmarker.user.service.UserContext;
@@ -26,23 +26,30 @@ public class UserApiController {
     private final UserContext userContext;
 
     @PostMapping("/user")
-    public ResponseEntity createUser(@RequestBody UserRegisterDto userRegisterDto) {
+    public ResponseEntity<UserInfo> createUser(@RequestBody UserRegisterDto userRegisterDto) {
         User user = userRegisterDto.toEntity();
-        return ResponseEntity.ok(userService.createUser(user));
+        Optional<User> savedUser = userService.createUser(user);
+        return ResponseEntity.ok(UserInfo.from(savedUser.get()));
     }
 
-    @GetMapping("/user")
+    @GetMapping({"/user", "/user/{user_id}"})
     @PreAuthorize("hasAnyRole('USER')")
     public ResponseEntity<UserInfo> getUser(
-        @RequestParam(required = false, defaultValue = "") String id) {
+        @PathVariable(required = false) String user_id) {
         User currentUser = userContext.getCurrentUser();
-        if (id.isEmpty()) {
+        if (user_id == null) {
             return ResponseEntity.ok(UserInfo.from(currentUser));
         } else {
-            User user = userService.getUser(currentUser.getId(), id);
-            return ResponseEntity.ok(UserInfo.from(user));
+            if (currentUser.getRole().isAdmin()) {
+                User user = userService.getUser(user_id);
+                return ResponseEntity.ok(UserInfo.from(user));
+            } else {
+                User user = userService.getUserIfSameGroup(currentUser.getId(), user_id);
+                return ResponseEntity.ok(UserInfo.from(user));
+            }
         }
     }
+
 
     @GetMapping("/user/me")
     @PreAuthorize("hasAnyRole('USER')")
