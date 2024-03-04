b702e0fdb468ada189cd15c972562e081d51ffb6
seungminyi <seungminyi95@gmail.com>
2024-03-05 01:10:46 +0900

    fix: AccessDeniedException Handling in Security Tests

--- bm-controller/src/main/java/org/benchmarker/common/controller/GlobalRestControllerAdvice.java
+++ bm-controller/src/main/java/org/benchmarker/common/controller/GlobalRestControllerAdvice.java
@@ -1,9 +1,13 @@
 package org.benchmarker.common.controller;
 
 import lombok.RequiredArgsConstructor;
+
+import org.benchmarker.common.error.ErrorCode;
 import org.benchmarker.common.error.GlobalErrorResponse;
 import org.benchmarker.common.error.GlobalException;
+import org.benchmarker.security.BMAccessDeniedHandler;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,5 +19,10 @@ public class GlobalRestControllerAdvice {
     @ExceptionHandler(GlobalException.class)
     public ResponseEntity<GlobalErrorResponse> handleGlobalException(GlobalException e) {
         return GlobalErrorResponse.toResponseEntity(e);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<GlobalErrorResponse> handlerResponseEntity(AccessDeniedException e) {
+        return GlobalErrorResponse.toResponseEntity(ErrorCode.FORBIDDEN);
     }
 }
