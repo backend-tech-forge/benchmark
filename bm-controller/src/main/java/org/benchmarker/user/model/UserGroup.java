dd3f2df7d20d4321138b5f28c92f5a93af09fa2a
Hwangbo <ghkdqhrbals@naver.com>
2024-03-05 20:48:19 +0900

    dev: Builder default set with Lombok:1.18.2

--- bm-controller/src/main/java/org/benchmarker/user/model/UserGroup.java
+++ bm-controller/src/main/java/org/benchmarker/user/model/UserGroup.java
@@ -3,31 +3,24 @@ package org.benchmarker.user.model;
 import jakarta.persistence.*;
 import lombok.*;
 import org.benchmarker.common.model.BaseTime;
+import org.benchmarker.user.constant.UserConsts;
 
 @Entity
 @Setter
 @Getter
+@Builder
+@ToString
+@AllArgsConstructor
 @NoArgsConstructor
 public class UserGroup extends BaseTime {
 
     @Id
-    private String id;
+    @Builder.Default
+    private String id = UserConsts.USER_GROUP_DEFAULT_ID;
+
     @Column(name = "group_name")
-
-    private String name;
-
-    @Builder
-    public UserGroup(String id, String name) {
-        this.id = id;
-        this.name = name;
-
-        if (this.name == null) {
-            this.name = "default";
-        }
-        if (this.id == null) {
-            this.id = "default";
-        }
-    }
+    @Builder.Default
+    private String name = UserConsts.USER_GROUP_DEFAULT_NAME;
 
 
 }
