c4f96fd350c413475915bad80242dc15f5afb6b9
Hwangbo <ghkdqhrbals@naver.com>
2024-03-05 20:48:06 +0900

    dev: Builder default set with Lombok:1.18.2

--- bm-controller/src/main/java/org/benchmarker/user/model/User.java
+++ bm-controller/src/main/java/org/benchmarker/user/model/User.java
@@ -1,9 +1,13 @@
 package org.benchmarker.user.model;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.benchmarker.common.model.BaseTime;
 
@@ -13,77 +17,35 @@ import org.benchmarker.common.model.BaseTime;
 @Getter
 @Entity
 @Table(name = "APP_USER")
+@Builder
+@ToString
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class User extends BaseTime {
 
     @Id
     private String id;
     private String password;
     @Column(name = "slack_webhook_url")
-    private String slackWebhookUrl;
+    @Builder.Default
+    private String slackWebhookUrl = "";
     @Column(name = "slack_notification")
-
     @Builder.Default
     private Boolean slackNotification = false;
     @Column(name = "email")
-    private String email;
+    @Builder.Default
+    private String email = "";
     @Column(name = "email_notification")
     @Builder.Default
     private Boolean emailNotification = false;
 
     @Column(name = "role")
     @Enumerated(EnumType.STRING)
-    private Role role;
+    @Builder.Default
+    private Role role = Role.ROLE_USER;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "group_id", referencedColumnName = "id", nullable = false)
     private UserGroup userGroup;
 
-
-    @Builder
-    public User(String id, String password, String slackWebhookUrl, Boolean slackNotification,
-        String email, Boolean emailNotification, Role role, UserGroup userGroup) {
-        this.id = id;
-        this.password = password;
-        this.slackWebhookUrl = slackWebhookUrl;
-        this.slackNotification = slackNotification;
-        this.email = email;
-        this.emailNotification = emailNotification;
-        this.role = role;
-        this.userGroup = userGroup;
-
-        if (this.slackWebhookUrl == null) {
-            this.slackWebhookUrl = "";
-        }
-
-        if (this.email == null) {
-            this.email = "";
-        }
-
-        if (this.role == null) {
-            this.role = Role.ROLE_USER;
-        }
-        if (this.slackNotification == null) {
-            this.slackNotification = false;
-        }
-        if (this.emailNotification == null) {
-            this.emailNotification = false;
-        }
-    }
-
-    public User() {
-
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-            "id='" + id + '\'' +
-            ", password='" + password + '\'' +
-            ", slackWebhookUrl='" + slackWebhookUrl + '\'' +
-            ", slackNotification=" + slackNotification +
-            ", email='" + email + '\'' +
-            ", emailNotification=" + emailNotification +
-            ", role=" + role +
-            '}';
-    }
 }
