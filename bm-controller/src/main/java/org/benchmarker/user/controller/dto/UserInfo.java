747804ad384596c32607c5bf4f73876f538885d7
Hwangbo <ghkdqhrbals@naver.com>
2024-03-08 21:22:32 +0900

    dev: UserInfo contain user group list

--- bm-controller/src/main/java/org/benchmarker/user/controller/dto/UserInfo.java
+++ bm-controller/src/main/java/org/benchmarker/user/controller/dto/UserInfo.java
@@ -2,14 +2,19 @@ package org.benchmarker.user.controller.dto;
 
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.benchmarker.user.model.User;
+import org.benchmarker.user.model.UserGroup;
+import org.benchmarker.user.model.UserGroupJoin;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Setter
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class UserInfo {
 
     @JsonProperty("id")
@@ -22,17 +27,29 @@ public class UserInfo {
     private String email;
     @JsonProperty("email_notification")
     private Boolean emailNotification;
+    @JsonProperty("groups")
+    private List<UserGroup> userGroup;
 
     @Builder
     public UserInfo(String id, String slackWebhookUrl, Boolean slackNotification, String email,
-        Boolean emailNotification) {
+        Boolean emailNotification, List<UserGroup> userGroup) {
         this.id = id;
         this.slackWebhookUrl = slackWebhookUrl;
         this.slackNotification = slackNotification;
         this.email = email;
         this.emailNotification = emailNotification;
+        this.userGroup = userGroup;
     }
 
+
+    /**
+     * Convert User to UserInfo
+     *
+     * Need to run in the same transaction
+     *
+     * @param user
+     * @return
+     */
     public static UserInfo from(User user) {
         return UserInfo.builder()
             .id(user.getId())
@@ -40,6 +57,7 @@ public class UserInfo {
             .slackNotification(user.getSlackNotification())
             .email(user.getEmail())
             .emailNotification(user.getEmailNotification())
+            .userGroup(user.getUserGroupJoin().stream().map(UserGroupJoin::getUserGroup).toList())
             .build();
     }
 }
