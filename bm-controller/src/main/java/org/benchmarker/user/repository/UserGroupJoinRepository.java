985c878de6ba70f921e42530cf5d943cb53cdb87
Hwangbo <ghkdqhrbals@naver.com>
2024-03-08 21:17:44 +0900

    dev: implement join-table repo for user & group

---
+++ bm-controller/src/main/java/org/benchmarker/user/repository/UserGroupJoinRepository.java
@@ -0,0 +1,16 @@
+package org.benchmarker.user.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.benchmarker.user.model.User;
+import org.benchmarker.user.model.UserGroup;
+import org.benchmarker.user.model.UserGroupJoin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserGroupJoinRepository extends JpaRepository<UserGroupJoin, Long>{
+
+    List<UserGroupJoin> findByUserId(String userId);
+    Optional<UserGroupJoin> findByUserAndUserGroup(User user, UserGroup userGroup);
+
+}
