1457a04e02041b74c49c5ab24038469a3452518b
Hwangbo <ghkdqhrbals@naver.com>
2024-03-08 21:18:42 +0900

    chore: add some default test consts

--- bm-controller/src/test/java/org/benchmarker/user/controller/constant/TestUserConsts.java
+++ bm-controller/src/test/java/org/benchmarker/user/controller/constant/TestUserConsts.java
@@ -1,13 +1,13 @@
 package org.benchmarker.user.controller.constant;
 
 public interface TestUserConsts {
+
     String id = "test_id";
     String password = "test_password";
     String email = "test_email@gmail.com";
     String slackWebhookUrl = "test_slack_webhook_url";
-    Boolean emailAlert = true;
-    Boolean slackAlert = true;
     String groupId = "test_group_id";
-
-
+    String groupName = "test_group_name";
+    Boolean slackNotification = false;
+    Boolean emailNotification = false;
 }
