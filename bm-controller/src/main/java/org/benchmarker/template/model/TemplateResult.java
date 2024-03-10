3b61f76ef42f8b50bd3c964feff65a5833ca694f
LeeJeongGi <apple4rhk@naver.com>
2024-03-11 06:41:59 +0900

    chore : Constructor annotation change

--- bm-controller/src/main/java/org/benchmarker/template/model/TemplateResult.java
+++ bm-controller/src/main/java/org/benchmarker/template/model/TemplateResult.java
@@ -1,18 +1,19 @@
 package org.benchmarker.template.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
+import org.benchmarker.common.model.BaseTime;
 import org.benchmarker.user.model.UserGroup;
-
-import java.time.LocalDateTime;
 
 @Slf4j
 @Getter
 @Setter
 @Entity
-public class TemplateResult {
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TemplateResult extends BaseTime {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,11 +37,4 @@ public class TemplateResult {
 
     private Integer mttbfb_avg;
 
-    @Column(columnDefinition = "timestamp(6)")
-    protected LocalDateTime createdAt;
-
-    @Column(columnDefinition = "timestamp(6)")
-    protected LocalDateTime updatedAt;
-
-    public TemplateResult() {}
 }
