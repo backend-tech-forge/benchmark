dcfc6845d1ef83d1749c03c99287a52d7ea4af51
Hwangbo <ghkdqhrbals@naver.com>
2024-03-10 16:26:50 +0900

    dev: initialize admin and defaultgroup

--- bm-controller/src/main/java/org/benchmarker/prerun/DataLoader.java
+++ bm-controller/src/main/java/org/benchmarker/prerun/DataLoader.java
@@ -2,6 +2,7 @@ package org.benchmarker.prerun;
 
 import jakarta.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.benchmarker.user.model.Role;
 import org.benchmarker.user.model.User;
@@ -15,6 +16,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import static org.benchmarker.common.util.NoOp.noOp;
 import static org.benchmarker.user.constant.UserConsts.USER_GROUP_DEFAULT_ID;
 import static org.benchmarker.user.constant.UserConsts.USER_GROUP_DEFAULT_NAME;
 
@@ -41,14 +43,28 @@ public class DataLoader implements CommandLineRunner {
     @Transactional
     public void run(String... args) throws Exception {
         // 데이터베이스에 초기 사용자 추가
-        UserGroup group = userGroupRepository.save(defaultUserGroup());
-        User user = userRepository.save(adminUser());
-        userGroupJoinRepository.save(
-            UserGroupJoin.builder()
-                .user(user)
-                .userGroup(group)
-                .build()
-        );
+        Optional<User> adminUser = userRepository.findById(adminId);
+        Optional<UserGroup> defaultGroup = userGroupRepository.findById(USER_GROUP_DEFAULT_ID);
+        adminUser.ifPresentOrElse(
+            user -> {
+                noOp();
+            },
+            () -> userRepository.save(adminUser()));
+        defaultGroup.ifPresentOrElse(
+            userGroup -> {
+                noOp();
+            },
+            () -> userGroupRepository.save(defaultUserGroup()));
+        List<UserGroupJoin> findJoin = userGroupJoinRepository.findByUserId(adminId);
+        if (findJoin.isEmpty()) {
+            Optional<UserGroup> group = userGroupRepository.findById(USER_GROUP_DEFAULT_ID);
+            group.ifPresent(userGroup -> userGroupJoinRepository.save(
+                UserGroupJoin.builder()
+                    .user(adminUser.get())
+                    .userGroup(defaultGroup.get())
+                    .build()
+            ));
+        }
     }
 
     private UserGroup defaultUserGroup() {
