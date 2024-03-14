7f30ac336a34deab902f022187f8f6692d318f06
Hwangbo <ghkdqhrbals@naver.com>
2024-03-15 07:31:59 +0900

    chore: add /api for restApi prefix url

--- bm-agent/src/main/java/org/benchmark/bmagent/controller/AgentApiController.java
+++ bm-agent/src/main/java/org/benchmark/bmagent/controller/AgentApiController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -18,6 +19,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 @Slf4j
 public class AgentApiController {
 
