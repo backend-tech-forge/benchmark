6fd56bfa6c2a2d206e2b21cb27a176013c544e74
Hwangbo Gyumin <ghkdqhrbals@naver.com>
2024-03-15 23:33:59 +0900

    feat: agent sse & scheduler management (#28)
    * dev: common dto module
    
    * dev: import common dto module
    
    * dev: import common dto module
    
    * dev: scheduler interface setup
    
    * dev: sse management interface setup
    
    * dev: declare common getStatus method in abstract class
    
    * dev: sse timeout consts implement to abstract class & set concurrentHashMap
    
    * dev: IOException global handler
    
    * dev: json string mapper utils
    
    * dev: scheduler status enum setup
    
    * dev: sse manage impl. class
    
    * dev: manage scheduling with given id
    
    * dev: restApis for agent, sse mapping
    
    * test: jsonMapper string to json test
    
    * test: test result dto setup
    
    * dev: add json library
    
    * chore: add /api for restApi prefix url
    
    * test: generate default testResults
    
    * dev: change String to Object in sse
    
    * chore: add toString
    
    * chore: sse return TestResult stubs in every seconds
    
    * chore: change server port to 8081
    
    * chore: add main class

---
+++ bm-common/src/test/java/org/benchmark/bmcommon/BmCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.benchmark.bmcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
