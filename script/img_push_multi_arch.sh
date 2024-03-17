fe6fe623ec5d435faf33106a7de9ca292fb19ac1
Hwangbo Gyumin <ghkdqhrbals@naver.com>
2024-03-17 16:30:42 +0900

    feat: CI/CD to GKE with multi-arch image builder, bash script (#33)
    * dev: k8s agent deployment, service
    
    * dev: pv and pvc setup with gcp-pd
    
    * dev: controller deployment, service
    
    * dev: database deployment, service
    
    * dev: add Dockerfile for each module
    
    * dev: image builder and pusher script
    
    * dev: ingress pod and their routes
    
    * dev: ignore secrets
    
    * dev: gke continuous deploy workflow
    
    * fix: path rewrite #32
    
    * fix: remove / in COPY prefix path #32
    
    * fix: change use of build-push-action #32
    
    * fix: test buildx path #32
    
    * fix: rewrite Dockerfile path with -f option #32
    
    * fix: add . end of push #32
    
    * fix: change context path with : #32
    
    * fix: use support option of file, version : #32
    
    * fix: valid option reset : #32
    
    * fix: add Dockerfile to file option : #32
    
    * fix: remove context : #32
    
    * fix: add . prefix : #32
    
    * fix: add context and set default workdir : #32
    
    * fix: add ARG for dockerfile : #32
    
    * fix: add ARG for dockerfile : #32
    
    * fix: arg option to build-arg : #32
    
    * fix: remove default JAR_PATH : #32
    
    * fix: set default JAR_PATH : #32
    
    * fix: test echoing path : #32
    
    * fix: remove docker build push marketplace : #32
    
    * fix: add custom script for build multi-arch and push : #32
    
    * fix: add sh args for getting token & etc : #32
    
    * fix: remove QEMU and buildx plugin : #32
    
    * fix: sh to bash : #32
    
    * fix: image list extract with ghkdqhrbals prefix : #32
    
    * fix: builx -f option path redefine #32
    
    * fix: secret key #32
    
    * fix: jar_path call with {} #32
    
    * dev: change push to workflow_run develop branch

---
+++ script/img_push_multi_arch.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+DEFAULT_USER="ghkdqhrbals"
+DEFAULT_PLATFORM="linux/amd64,linux/arm64"
+DEFAULT_VERSION="latest"
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -u|--username)
+            BM_USER="$2"
+            shift
+            ;;
+        -p|--platform)
+            BM_PLATFORM="$2"
+            shift
+            ;;
+        -v|--version)
+            BM_VERSION="$2"
+            shift
+            ;;
+        -t|--token)
+            DOCKER_HUB_TOKEN="$2"
+            shift
+            ;;
+        *)
+            echo "Invalid argument: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+BM_USER=${BM_USER:-$DEFAULT_USER}
+BM_PLATFORM=${BM_PLATFORM:-$DEFAULT_PLATFORM}
+BM_VERSION=${BM_VERSION:-$DEFAULT_VERSION}
+
+# Check if required arguments are provided
+if [ -z "$BM_USER" ] || [ -z "$BM_PLATFORM" ] || [ -z "$BM_VERSION" ] || [ -z "$DOCKER_HUB_TOKEN" ]; then
+    echo "Usage: $0 [-u|--username <username>] [-p|--platform <platform>] [-v|--version <version>] [-t|--token <docker_hub_token>]"
+    exit 1
+fi
+
+# This is a script to push with multiple platform images to docker hub
+echo "Logging in to Docker Hub"
+docker login -u "$BM_USER" -p $DOCKER_HUB_TOKEN
+
+# Use Docker Buildx
+echo "Using Docker Buildx"
+docker buildx create --use
+docker buildx inspect --bootstrap
+
+# Build and push Docker images for multiple platforms
+echo "Build the docker image with multi platform support"
+docker buildx build --platform ${BM_PLATFORM} -t ${BM_USER}/bm-controller:${BM_VERSION} -f ./bm-controller/Dockerfile --push .
+docker buildx build --platform ${BM_PLATFORM} -t ${BM_USER}/bm-agent:${BM_VERSION} -f ./bm-agent/Dockerfile --push .
+
+# Push the docker image to docker hub
+images=$(docker images --format "{{.Repository}}" | grep "^${BM_USER}")
+echo "Image deploy to docker hub"
+for image in $images; do
+  echo "${image}"
+  docker push "${image}"
+done
