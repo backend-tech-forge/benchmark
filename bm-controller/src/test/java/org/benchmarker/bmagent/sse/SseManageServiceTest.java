d7de4d1d8a6549cad67a9b2a5dcc2ff77effa3a1
Hwangbo <ghkdqhrbals@naver.com>
2024-03-21 17:54:24 +0900

    dev: testing sse and scheduler thread

---
+++ bm-controller/src/test/java/org/benchmarker/bmagent/sse/SseManageServiceTest.java
@@ -0,0 +1,53 @@
+package org.benchmarker.bmagent.sse;
+
+import org.benchmark.dto.TestResult;
+import org.benchmark.util.RandomUtils;
+import org.benchmarker.bmagent.pref.ResultManagerService;
+import org.benchmarker.bmagent.schedule.ScheduledTaskService;
+import org.benchmarker.bmagent.schedule.SchedulerStatus;
+import org.benchmarker.bmagent.service.IScheduledTaskService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import static java.lang.Thread.sleep;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class SseManageServiceTest {
+    private IScheduledTaskService scheduledTaskService;
+    private ResultManagerService resultManagerService;
+
+    private SseManageService sseManageService;
+
+    @BeforeEach
+    void setUp() {
+        scheduledTaskService = new ScheduledTaskService();
+        resultManagerService = new ResultManagerService();
+        sseManageService = new SseManageService(scheduledTaskService, resultManagerService);
+    }
+
+    @Test
+    @DisplayName("sse 시작 시 SseEmitter와 ScheduledTask가 시작되고 셧다운 시, ScheduledTask 와 emitter 가 종료된다")
+    void start_ShouldStartSseEmitterAndScheduledTask() throws InterruptedException {
+        // given
+        Long id = 1L;
+        TestResult resultStub = RandomUtils.generateRandomTestResult();
+        resultManagerService.save(id, resultStub);
+
+        // when
+        SseEmitter result = sseManageService.start(id);
+
+        // then
+        assertThat(result).isNotNull();
+        assertThat(scheduledTaskService.getStatus().get(id)).isEqualTo(SchedulerStatus.RUNNING);
+
+        sseManageService.stop(id);
+        assertThat(scheduledTaskService.getStatus().get(id)).isNull();
+    }
+}
