982fafcd2dfe1e2b97981aafa909fdec7e045fae
Hwangbo <ghkdqhrbals@naver.com>
2024-03-26 18:42:25 +0900

    dev: add prepareScript & header field

--- bm-controller/src/main/java/org/benchmarker/bmcontroller/template/controller/dto/TestTemplateRequestDto.java
+++ bm-controller/src/main/java/org/benchmarker/bmcontroller/template/controller/dto/TestTemplateRequestDto.java
@@ -1,8 +1,15 @@
 package org.benchmarker.bmcontroller.template.controller.dto;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.benchmarker.bmcontroller.template.model.TestTemplate;
 import org.benchmarker.bmcontroller.user.model.UserGroup;
 
@@ -18,34 +25,33 @@ public class TestTemplateRequestDto {
 
     @NotBlank
     private String url;
-
     @NotBlank
     private String method;
-
     private String body;
-
     @NotNull
     private Integer vuser;
-
     @NotNull
     private Integer maxRequest;
-
+    // maxDuration is in seconds
     @NotNull
     private Integer maxDuration;
-
     @NotNull
     private Integer cpuLimit;
-  
-    public TestTemplate toEntity() {
+    private Map<String, Object> headers;
+    private String prepareScript;
+
+    public TestTemplate toEntity() throws JsonProcessingException {
         return TestTemplate.builder()
-                .userGroup(UserGroup.builder().id(this.userGroupId).build())
-                .url(this.url)
-                .method(this.method)
-                .body(this.body)
-                .vuser(this.vuser)
-                .maxRequest(this.maxRequest)
-                .maxDuration(this.maxDuration)
-                .cpuLimit(this.cpuLimit)
-                .build();
+            .userGroup(UserGroup.builder().id(this.userGroupId).build())
+            .url(this.url)
+            .method(this.method)
+            .body(this.body)
+            .headers(new ObjectMapper().writeValueAsString(this.headers))
+            .prepareScript(this.prepareScript)
+            .vuser(this.vuser)
+            .maxRequest(this.maxRequest)
+            .maxDuration(this.maxDuration)
+            .cpuLimit(this.cpuLimit)
+            .build();
     }
 }
