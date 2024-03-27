f24f8129f6a536d79de43364c2268e39deb0cb30
Hwangbo <ghkdqhrbals@naver.com>
2024-03-27 10:00:02 +0900

    dev: TPS, MTTFB percentile consts

---
+++ bm-agent/src/main/java/org/benchmarker/bmagent/consts/PreftestConsts.java
@@ -0,0 +1,9 @@
+package org.benchmarker.bmagent.consts;
+
+import java.util.Arrays;
+import java.util.List;
+
+public interface PreftestConsts {
+    List<Double> percentiles = Arrays.asList(50D, 90D, 95D,99D,99.9D);
+
+}
