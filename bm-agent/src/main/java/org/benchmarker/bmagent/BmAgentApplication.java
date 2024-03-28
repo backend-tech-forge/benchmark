37f0572f21abd2d39c1b326218e64a62e4c47835
Hwangbo <ghkdqhrbals@naver.com>
2024-03-29 07:04:56 +0900

    dev: exit code

--- bm-agent/src/main/java/org/benchmarker/bmagent/BmAgentApplication.java
+++ bm-agent/src/main/java/org/benchmarker/bmagent/BmAgentApplication.java
@@ -1,13 +1,24 @@
 package org.benchmarker.bmagent;
 
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BmAgentApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(BmAgentApplication.class, args);
+    @Bean
+    public ExitCodeGenerator exitCodeGenerator() {
+        return new ExitCodeGenerator() {
+            @Override
+            public int getExitCode() {
+                return 42;
+            }
+        };
     }
 
+    public static void main(String[] args) {
+        System.setProperty("spring.application.name","bm-agent");
+        SpringApplication.run(BmAgentApplication.class, args);
+    }
 }
