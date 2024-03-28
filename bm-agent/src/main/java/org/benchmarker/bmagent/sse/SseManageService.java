8ab500ed1a807ce8e2c7d827b50104039c673549
Hwangbo <ghkdqhrbals@naver.com>
2024-03-29 07:07:36 +0900

    dev: append agentStatusManager to constructor

--- bm-agent/src/main/java/org/benchmarker/bmagent/sse/SseManageService.java
+++ bm-agent/src/main/java/org/benchmarker/bmagent/sse/SseManageService.java
@@ -61,7 +61,7 @@ public class SseManageService extends AbstractSseManageService {
         // Save the SseEmitter to the map
         sseEmitterHashMap.put(id, emitter);
 
-        httpSender.put(id, new HttpSender(resultManagerService, scheduledTaskService));
+        httpSender.put(id, new HttpSender(resultManagerService, scheduledTaskService, agentStatusManager));
         HttpSender htps = httpSender.get(id);
 
         LocalDateTime now = LocalDateTime.now();
