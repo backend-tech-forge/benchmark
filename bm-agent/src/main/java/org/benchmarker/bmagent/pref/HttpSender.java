cc0ef032895bd4bbc8ed68d406b3e7dfcc2e6300
Hwangbo <ghkdqhrbals@naver.com>
2024-03-28 20:24:59 +0900

    dev: maxRequest maxDuration validator

--- bm-agent/src/main/java/org/benchmarker/bmagent/pref/HttpSender.java
+++ bm-agent/src/main/java/org/benchmarker/bmagent/pref/HttpSender.java
@@ -1,7 +1,10 @@
 package org.benchmarker.bmagent.pref;
 
+import static org.benchmarker.bmcommon.util.NoOp.noOp;
+
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Comparator;
@@ -22,6 +25,8 @@ import reactor.core.publisher.Mono;
 
 /**
  * High load HTTP sender
+ *
+ * @author Gyumin Hwangbo
  */
 @Slf4j
 @Getter
@@ -37,6 +42,8 @@ public class HttpSender {
         this.scheduledTaskService = scheduledTaskService;
     }
 
+    private Integer defaultMaxRequestsPerUser = 100000000;
+    private Integer defaultMaxDuration = 5; // 5 hours
     private AtomicInteger totalRequests = new AtomicInteger(0);
     private AtomicInteger totalSuccess = new AtomicInteger(0);
     private AtomicInteger totalErrors = new AtomicInteger(0);
@@ -46,14 +53,18 @@ public class HttpSender {
     private ConcurrentHashMap<LocalDateTime, Long> mttfbMap = new ConcurrentHashMap<>();
     private ConcurrentHashMap<String, Integer> statusCodeCount = new ConcurrentHashMap<>();
     private List<CompletableFuture<Void>> futures;
-    private Boolean isRunning=true;
+    private Boolean isRunning = true;
 
     /**
-     * Send multiple requests to target server
+     * <strong>Major implementation sending multiple requests to target server</strong>
+     *
+     * <p>Whenever totalRequests hit maxRequests, test stop even if duration is still remained.</p>
+     * <p>But, if you has null value of maxRequests and valid maxDuration, it will continue to run
+     * test until current time is reach to maxDuration </p>
      *
      * <p>This method run child scheduler with {@link TemplateInfo#id} key</p>
      *
-     * @param templateInfo
+     * @param templateInfo {@link TemplateInfo}
      */
     public void sendRequests(TemplateInfo templateInfo) throws MalformedURLException {
         URL url = new URL(templateInfo.getUrl());
@@ -61,15 +72,30 @@ public class HttpSender {
             templateInfo.getUrl(),
             templateInfo.getBody(),
             templateInfo.getHeaders());
-        // Future 설정
+
+        // if both duration & requests is not valid, immediately return
+        if (templateInfo.getMaxDuration() == null && templateInfo.getMaxRequest() == null) {
+            return;
+        } else if (templateInfo.getMaxRequest() != null && templateInfo.getMaxDuration() == null) {
+            templateInfo.setMaxDuration(Duration.ofHours(defaultMaxDuration));
+        } else if (templateInfo.getMaxRequest() == null && templateInfo.getMaxDuration() != null) {
+            templateInfo.setMaxRequest(defaultMaxRequestsPerUser);
+        } else {
+            noOp();
+        }
+
+        Duration duration = templateInfo.getMaxDuration();
+
+        // Future setup
         futures = IntStream.range(0, templateInfo.getVuser())
             .mapToObj(i -> CompletableFuture.runAsync(() -> {
+                long startTime = System.currentTimeMillis(); // 시작 시간 기록
+                long endTime = startTime + duration.toMillis();
                 for (int j = 0; j < templateInfo.getMaxRequest(); j++) {
-                    if (!isRunning){
+                    // 만약 running 이 아니거나 시간이 끝났다면,
+                    if (!isRunning || System.currentTimeMillis() > endTime) {
                         return;
                     }
-                    // HTTP 전송 -> target server
-
                     long requestStartTime = System.currentTimeMillis();  // 요청 시작 시간 기록
                     req.exchangeToMono(resp -> {
                         String statusCode = resp.statusCode().toString();
@@ -82,23 +108,28 @@ public class HttpSender {
                         return Mono.empty();
                     }).block();
 
-                    long requestEndTime = System.currentTimeMillis(); // 요청 종료 시간 기록
-                    long elapsedTime = requestEndTime - requestStartTime; // 요청 지연 시간 계산
-                    LocalDateTime currentTime = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
-                    mttfbMap.merge(currentTime, elapsedTime, (oldValue, newValue) -> (oldValue + newValue) / 2); // 1초 간격 time series로 계산된 평균 지연 시간 저장
+                    long requestEndTime = System.currentTimeMillis();
+                    long elapsedTime = requestEndTime - requestStartTime;
+                    LocalDateTime currentTime = LocalDateTime.now()
+                        .truncatedTo(ChronoUnit.SECONDS);
+                    mttfbMap.merge(currentTime, elapsedTime,
+                        (oldValue, newValue) -> (oldValue + newValue) / 2);
                     tps.incrementAndGet();
                     totalRequests.incrementAndGet();
                 }
+
             }))
             .toList();
 
         // Need to calculate & save TPS and MTTFB in every 1 second.
-        scheduledTaskService.startChild(Long.valueOf(templateInfo.getId()),"recorder", () -> {
+        scheduledTaskService.startChild(Long.valueOf(templateInfo.getId()), "recorder", () -> {
             // save current tps & reset
-            tpsMap.put(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), Double.valueOf(tps.get()));
+            tpsMap.put(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS),
+                Double.valueOf(tps.get()));
             tps.set(0); // initial
         }, 0, 1, java.util.concurrent.TimeUnit.SECONDS);
 
+        // CompletableFuture 종료까지 대기
         CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
     }
 
@@ -108,17 +139,17 @@ public class HttpSender {
      * @param percentile The percentile to calculate (e.g., 90 for 90th percentile)
      * @return The calculated percentile value
      */
-    public Map<Double,Double> calculateTpsPercentile(List<Double> percentile) {
-        Map<Double,Double> result = new HashMap<>();
+    public Map<Double, Double> calculateTpsPercentile(List<Double> percentile) {
+        Map<Double, Double> result = new HashMap<>();
 
         List<Double> tpsList = this.sortedTpsMap();
         int size = tpsList.size();
-        for (Double p : percentile){
+        for (Double p : percentile) {
             int index = (int) Math.ceil((p / 100) * size) - 1;
             if (index < 0) {
-                return Map.of(0D,0D); // 예외처리: 인덱스가 음수인 경우
+                return Map.of(0D, 0D); // 예외처리: 인덱스가 음수인 경우
             }
-            result.put(p,tpsList.get(index));
+            result.put(p, tpsList.get(index));
         }
 
         return result;
@@ -126,8 +157,10 @@ public class HttpSender {
 
     public List<Double> sortedTpsMap() {
         Map<LocalDateTime, Double> tpsSnapshot = new ConcurrentHashMap<>(tpsMap);
-        return tpsSnapshot.values().stream().sorted(Comparator.reverseOrder()).toList(); // <-- Here STOP!
+        return tpsSnapshot.values().stream().sorted(Comparator.reverseOrder())
+            .toList(); // <-- Here STOP!
     }
+
     public List<Long> sortedMttfbMap() {
         Map<LocalDateTime, Long> tpsSnapshot = new ConcurrentHashMap<>(mttfbMap);
         return tpsSnapshot.values().stream().sorted().toList(); // <-- Here STOP!
@@ -139,15 +172,15 @@ public class HttpSender {
      * @param percentile The percentile to calculate (e.g., 90 for 90th percentile)
      * @return The calculated percentile value
      */
-    public Map<Double,Double> calculateMttfbPercentile(List<Double> percentile) {
-        Map<Double,Double> result = new HashMap<>();
+    public Map<Double, Double> calculateMttfbPercentile(List<Double> percentile) {
+        Map<Double, Double> result = new HashMap<>();
 
         List<Long> mttfbList = this.sortedMttfbMap();
         int size = mttfbList.size();
-        for (Double p : percentile){
+        for (Double p : percentile) {
             int index = (int) Math.ceil((p / 100) * size) - 1;
             if (index < 0) {
-                return Map.of(0D,0D); // 예외처리: 인덱스가 음수인 경우
+                return Map.of(0D, 0D); // 예외처리: 인덱스가 음수인 경우
             }
             result.put(p, Double.valueOf(mttfbList.get(index)));
         }
@@ -159,7 +192,7 @@ public class HttpSender {
      * downstream 에서만 제거 가능. 외부에서는 cancel 불가능...
      */
     public void cancelRequests() {
-        isRunning=false;
+        isRunning = false;
 
     }
 
