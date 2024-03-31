16757721e69675d166c152a2dbd65ca4ba11599a
Hwangbo <ghkdqhrbals@naver.com>
2024-04-01 05:25:13 +0900

    dev: setup stomp with SockJS

--- bm-controller/src/main/java/org/benchmarker/bmcontroller/preftest/websocket/WebSocketConfig.java
+++ bm-controller/src/main/java/org/benchmarker/bmcontroller/preftest/websocket/WebSocketConfig.java
@@ -18,7 +18,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket");
+        registry.addEndpoint("/gs-guide-websocket")
+            .setAllowedOrigins("http://localhost:8080")
+            .withSockJS();
     }
 
 }
