90c8ac5d3d509522c58c98dd1107535290fb3ed6
Hwangbo <ghkdqhrbals@naver.com>
2024-04-10 17:26:10 +0900

    dev: randomized interface and generics

--- bm-controller/src/main/java/org/benchmarker/bmcontroller/preftest/common/TestInfo.java
+++ bm-controller/src/main/java/org/benchmarker/bmcontroller/preftest/common/TestInfo.java
@@ -1,6 +1,7 @@
 package org.benchmarker.bmcontroller.preftest.common;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,6 +9,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.benchmarker.bmagent.AgentStatus;
+import org.benchmarker.bmcommon.util.RandomUtils;
+import org.benchmarker.util.Randomized;
 
 @Getter
 @Setter
@@ -15,10 +18,22 @@ import org.benchmarker.bmagent.AgentStatus;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class TestInfo {
+public class TestInfo implements Randomized<TestInfo> {
+
     private String groupId;
     private Integer templateId;
     private String testId;
     private AgentStatus testStatus;
     private LocalDateTime startedAt;
+
+    @Override
+    public TestInfo random() {
+        return TestInfo.builder()
+            .testId(UUID.randomUUID().toString())
+            .startedAt(LocalDateTime.now())
+            .testStatus(AgentStatus.TESTING)
+            .templateId(RandomUtils.randInt(0, 100))
+            .groupId("defaultGroupId")
+            .build();
+    }
 }
