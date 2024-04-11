076d148f461f07888584d4d2f2467310f6182d3a
Hwangbo <ghkdqhrbals@naver.com>
2024-04-11 18:26:40 +0900

    test: discovery service & scheduler test

--- bm-controller/src/main/java/org/benchmarker/bmcontroller/agent/AgentServerManager.java
+++ bm-controller/src/main/java/org/benchmarker/bmcontroller/agent/AgentServerManager.java
@@ -13,35 +13,45 @@ import org.springframework.stereotype.Component;
 @Getter
 @Slf4j
 public class AgentServerManager {
+
+    // store current status of all agents
     private final ConcurrentHashMap<String, AgentInfo> agentsUrl = new ConcurrentHashMap<>();
+
+    // key: running templateId, value: relate agent
     private final HashMap<Long, String> agentMapped = new HashMap<>();
+
     public void add(String url, AgentInfo agentInfo) {
         agentsUrl.put(url, agentInfo);
     }
-    public void update(AgentInfo agentInfo){
-        if (agentsUrl.get(agentInfo.getServerUrl())==null){
+
+    public void update(AgentInfo agentInfo) {
+        if (agentsUrl.get(agentInfo.getServerUrl()) == null) {
             log.error("cannot find agent");
             return;
         }
         agentsUrl.put(agentInfo.getServerUrl(), agentInfo);
     }
 
-    public Optional<AgentInfo> getReadyAgent(){
-        for (AgentInfo agentInfo : agentsUrl.values()){
-            if (agentInfo.getStatus()== AgentStatus.READY){
+    public Optional<AgentInfo> getReadyAgent() {
+        for (AgentInfo agentInfo : agentsUrl.values()) {
+            if (agentInfo.getStatus() == AgentStatus.READY) {
                 return Optional.of(agentInfo);
             }
         }
         return Optional.empty();
     }
 
-    public void addTemplateRunnerAgent(Long id, String url){
-        agentMapped.put(id,url);
+    public void remove(String url) {
+        agentsUrl.remove(url);
     }
 
-    public void removeTemplateRunnerAgent(Long id){
+    public void addTemplateRunnerAgent(Long id, String url) {
+        agentMapped.put(id, url);
+    }
+
+    public void removeTemplateRunnerAgent(Long id) {
         String url = agentMapped.get(id);
-        if (url != null){
+        if (url != null) {
             agentMapped.remove(id);
             agentsUrl.remove(url);
         }
