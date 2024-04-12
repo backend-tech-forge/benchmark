70859f66fb64ac81bf265c2b2c66fbf0f8fb7229
Hwangbo <ghkdqhrbals@naver.com>
2024-04-12 19:07:46 +0900

    dev: dto for MTTFB, TPS

---
+++ bm-common/src/main/java/org/benchmarker/bmcommon/dto/TPSInfo.java
@@ -0,0 +1,20 @@
+package org.benchmarker.bmcommon.dto;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class TPSInfo {
+    private LocalDateTime timestamp;
+    private double tps;
+}
